7967130bde60e228e2b9de4c7737d2e4be7bf5a2
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-03-24 10:41:50 +0700

    Add files via upload

---
+++ components/Singer.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class Singer extends Component {
+    constructor(props){
+        super(props);
+        this.state ={singers : []}
+    }
+    render() {
+        return (
+        <View style={{flex : 1 , justifyContent : 'center'}}>
+            <View style={{ alignItems : 'center' , padding : 20}}>
+                <Text style={{fontSize : 20  }}> Singers Component </Text>
+            </View>
+            <FlatList
+                data={this.state.singers}
+                keyExtractor={item => item.id}
+                renderItem={param => (
+                    <View style={{ marginTop : 20 , marginHorizontal : 10 , padding : 10, flexDirection : 'row' , backgroundColor : '#EEE9F6'}}>
+                        <View style={{ margin : 5}}  >
+                            <Image
+                                style={{width : 100 , height : 100}} >
+                            </Image>
+                        </View>
+                        <View style={{ justifyContent : 'space-around' }}>
+                            <Text style={{fontWeight : 'bold' , fontSize : 20}}>Ca si</Text> 
+                            <Text style={{fontWeight : 'normal' , fontSize : 15}}>Email</Text> 
+                        </View>
+                           
+                    </View>
+                )}
+            >
+            </FlatList>
+        </View>
+        );
+    }
+}
